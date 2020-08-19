fa2174e63961a906d166bb3c96bccd6f01e0d851
Nuthan Chandra <nuthanc@juniper.net>
2020-08-19 11:40:15 +0530

    Update failure tc

--- failure_tc.py
+++ failure_tc.py
@@ -4,8 +4,7 @@ import re
 #variables
 
 # target URL to scrap
-url = "http://10.204.216.50/Docs/logs/1911-1_2019_10_22_19_06_42/junit-noframes.html"
-
+url = input("Enter url to parse:")
 # headers
 headers = {
     'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
@@ -15,28 +14,25 @@ headers = {
 response = requests.request("GET", url, headers=headers)
 
 # parse the downloaded data
-data = BeautifulSoup(response.text, 'html.parser')
-# print(data.find("tr",{"class":"Error"}).prettify()) 
-error_tc = data.find_all("tr", {"class": "Error"})
-print(len(error_tc))
-for case in data.find_all("tr", {"class": "Error"}):
-    fm = (case.find('td').text).split("[")[0].split(".")[1]
-    print(fm)
+def parse_data():
+    data = BeautifulSoup(response.text, 'html.parser')
+    # print(data.find("tr",{"class":"Error"}).prettify()) 
+    error_class_html = data.find_all("tr", {"class": "Error"})
+    with open('log.html','w') as f:
+        f.write(str(error_class_html))
+    print(len(error_class_html))
+    for error in error_class_html:
+        case = (error.find_all('td'))
+        test_case_with_class = case[0].text
+        test_case = case[0].text.split("[")[0].split(".")[1]
+        # import pdb;pdb.set_trace()
+        if test_case.startswith('test'):
+            loc = re.findall(r'/contrail-test/s.*?py', case[2].text)[0]
+        else:
+            loc = re.findall(r'/contrail-test/.*?py', case[2].text)[0]
+        print(test_case)
+        print("Test case with class:", test_case_with_class)
+        print("Location of test case: ",loc)
+        print()
 
-print("\nLocation of test cases\n")
-test_cases = set()
-
-
-for code in data.find_all("code"):
-    cases = re.findall(r'/contrail-test/s.*py',code.text)
-    for case in cases:
-        test_cases.add(case)
-        
-for tc in test_cases:
-    print(tc)
-
-
-# with open('error_tc.html','w') as f:
-#     for case in data.find_all("tr", {"class": "Error"}):
-#         f.write(case.prettify())
-
+parse_data()
